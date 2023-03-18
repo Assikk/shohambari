d819daf2c5f38bda0bc2960382711a8a38440d22
Asliddin <assikk1997@gmail.com>
2023-03-18 17:45:20 +0500

    create project

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex min-h-screen items-center">
+    <div class="max-w-[448px] mx-auto">
+      <img width="448" height="80" src="logo.svg" alt="Logo">
+      <form class="mt-8 border border-[#D1D5DB] rounded-lg py-8 px-10">
+        <h1 class="text-center text-2xl font-bold">
+          Авторизация
+        </h1>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
